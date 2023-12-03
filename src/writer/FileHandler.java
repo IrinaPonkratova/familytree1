b35f217a896e3adddd5680ff1f5986156c526f2a
Irina <ira.ponkratova97@mail.ru>
2023-12-03 18:57:23 +0300

    Заполнение FileHandler и Writable, делаем класс FamilyTree сериализуемым объектом

--- src/writer/FileHandler.java
+++ src/writer/FileHandler.java
@@ -1,4 +1,28 @@
-package writer;
+package ru.gb.family_tree.writer;
 
-public class FileHandler {
+
+import ru.gb.family_tree.Human;
+
+import java.io.*;
+
+public class FileHandler implements ru.gb.family_tree.writer.Writable {
+    public  boolean save(Serializable serializable, String path) {
+        try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(path))) {
+            objectOutputStream.writeObject(serializable);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Object read(String path) {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(path))) {
+            return objectInputStream.readObject();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- src/writer/Writable.java
+++ src/writer/Writable.java
@@ -1,4 +1,12 @@
-package writer;
+package ru.gb.family_tree.writer;
+
+import ru.gb.family_tree.Human;
+
+
+import java.io.Serializable;
 
 public interface Writable {
+    boolean save(Serializable serializable, String path);
+
+    Object read(String path);
 }
